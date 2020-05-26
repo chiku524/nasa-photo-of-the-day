714d9afc0a0bd8fdde0b62352d88a8bdfb4c0191
Nico Chikuji <chiku524@icloud.com>
2020-05-26 00:46:52 -0400

    imported axios and put in a useeffect with it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => { 
+    const fetchData = () => {
+      axios.get('https://api.nasa.gov/planetary/apod?api_key=L7cYeMNDJ6AmPKxk8hI3SPfW7AfXjN711ys0NNjH')
+        .then(response => {
+          setData(response.data)
+          console.log(data);
+        })
+        .catch(error => console.log(error));
+    }
+
+    fetchData();
+  }, [])
+
+
   return (
     <div className="App">
       <p>
