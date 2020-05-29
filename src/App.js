3559b92b8c1e3cd735e8062af5e8a733609aff93
Nico Chikuji <chiku524@icloud.com>
2020-05-29 00:08:27 -0400

    so close to making the photo buttons work

--- src/App.js
+++ src/App.js
@@ -5,20 +5,58 @@ import NavBar from './NavBar';
 import POTD from './PhotoOfTheDay';
 import Title from './Title';
 import Explanation from './Explanation';
-import Btn from './Button';
 
 function App() {
+  var today = new Date();
+  var month = (today.getMonth() + 1);
+  var year = today.getFullYear();
+  var day = today.getDate();
+  var dateFormatted = year + '-0' + month + '-' + day;
+  console.log(dateFormatted);
+
+  //pass in a function to POTD
+  function leftClick(){
+    day=day-1;
+    if(day.lenth < 2){
+      day = '0' + (day-1);
+    }
+    if(month.length < 2){
+      month = '0'
+    }
+    var dateFormatted = year + '-0' + month + '-' + day;
+    setDate(dateFormatted);
+    console.log(dateFormatted);
+  }
+
+  function rightClick(){
+    day=day+1;
+    if(day.length < 2){
+      day = '0' + (day + 1);
+    }
+    if(month.length < 2){
+      month = '0'
+    }
+    var dateFormatted = year + '-0' + month + '-' + day;
+    setDate(dateFormatted);
+  }
+
   const [data, setData] = useState({hey: 'hey'});
-  const [date, setDate] = useState('hey');
+  const [date, setDate] = useState('2020-05-29');
   const [title, setTitle] = useState('Hey');
   const [explanation, setExplanation] = useState('bye');
 
   useEffect(() => { 
     const fetchData = () => {
-      axios.get('https://api.nasa.gov/planetary/apod?api_key=L7cYeMNDJ6AmPKxk8hI3SPfW7AfXjN711ys0NNjH')
+      // if (month.length < 2) {
+      //   month = '0' + month;
+      // }
+      // if (day.length < 2) {
+      //   day = '0' + day;      
+      // }
+       axios.get(`https://api.nasa.gov/planetary/apod?api_key=L7cYeMNDJ6AmPKxk8hI3SPfW7AfXjN711ys0NNjH&date=${date}`)
         .then(response => {
           setData(response.data);
-          setDate(response.data.date);
+          setDate(dateFormatted);
           setTitle(response.data.title);
           setExplanation(response.data.explanation);
         })
@@ -26,7 +64,7 @@ function App() {
     }
 
     fetchData();
-  }, [date])
+  }, [dateFormatted, date])
 
   console.log(data);
 
@@ -39,9 +77,7 @@ function App() {
       </p> */}
       <Title title={title} date={date} />
       <div className='PicAndButtonContainer'>
-        <Btn>Left</Btn>
-        <POTD props={data} date={date} />
-        <Btn>Right</Btn>
+        <POTD props={data} date={date} leftClick={leftClick} rightClick={rightClick} />
       </div>
       <Explanation explanation={explanation} />
     </div>
--- src/PhotoOfTheDay.js
+++ src/PhotoOfTheDay.js
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
+import {Button} from 'reactstrap';
 
 const PictureOfTheDay = styled.img`
     max-width: 80%;
 `;
 
-function POTD({props, date}){
+function POTD({props, date, leftClick, rightClick}){
     const [picture, setPicture] = useState(props);
     
     useEffect(() => {
@@ -15,10 +16,21 @@ function POTD({props, date}){
     console.log(date);
     console.log(picture);
 
+    const style = {
+        height: '150px',
+        width: '200px',
+        margin: '0px 5%',
+        backgroundColor: 'black',
+        borderRadius: '300px',
+        color: 'white'
+    }
+
     if (!props.hdurl) return <h3>Loading...</h3>;
     else return (
         <div className='POTD'>
+            <Button style={style} onClick={leftClick} >Left</Button>
             <PictureOfTheDay src={picture} alt='IOTD'></PictureOfTheDay>
+            <Button style={style} onClick={rightClick} >Right</Button>
         </div>
     );
 }
